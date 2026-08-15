01b75d69a827b23230a86f89c639ee828ec7ead3
kill-ux <mustaphaboutoubdev@gmail.com>
2026-08-15 15:46:57 +0100

    fix: add unit tests

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -3,20 +3,6 @@ import pytest
 from pytest_mock import MockerFixture
 from app import create_app
 
-# @pytest.fixture
-# def client(monkeypatch, mocker: MockerFixture):
-#     # Set environment variables
-#     monkeypatch.setenv("INVENTORY_DB_USER", "inventory-db")
-#     monkeypatch.setenv("INVENTORY_DB_PASS", "inventory-db")
-#     monkeypatch.setenv("INVENTORY_DB_NAME", "inventory-db")
-#     monkeypatch.setenv("INVENTORY_DB_HOST", "inventory-db")
-
-#     mocker.patch("app.db.create_all")
-
-#     app = create_app()
-#     with app.app_context():
-#         yield app.test_client()
-
 @pytest.fixture
 def app(monkeypatch, mocker: MockerFixture):
     monkeypatch.setenv("INVENTORY_DB_USER", "inventory-db")
@@ -65,13 +51,6 @@ def mock_query(mocker):
     return mocker.Mock()
 
 
-@pytest.fixture
-def patch_movie_query(mocker, mock_query):
-    return mocker.patch.object(
-        _QueryProperty,
-        "__get__",
-        return_value=mock_query,
-    )
 
 @pytest.fixture
 def mock_add(mocker):
